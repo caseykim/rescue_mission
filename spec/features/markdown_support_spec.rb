1e8d9192addbd77faf39d82891dbc2ff1ea31ce5
Casey Kim <k.yesac@gmail.com>
2015-09-16 18:48:54 -0400

    Markdown v.1

---
+++ spec/features/markdown_support_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+feature "Markdown support", %(
+  As a user
+  I want to write my questions and answers with markdown
+  So I can easily format my questions and answers
+
+  Acceptance Criteria
+
+  [ ] I can write my questions and answers using markdown syntax
+  [ ] Questions and answers, when shown, should be the HTML rendered from the
+    markdown
+  ) do
+
+end
